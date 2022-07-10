d94f3e0977e32e4652f5e962f5709a24be39b5e8
Kovalchuk_Mikhail <kovalchuckmikhail@yandex.ru>
2022-07-10 15:54:35 +0300

    random added

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
 int result = numberA + numberB;
+Console.WriteLine("первое число равно " + numberA);
+Console.WriteLine("второе число равно " + numberB);
 Console.WriteLine(result);
